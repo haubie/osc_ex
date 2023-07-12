a17b14e873d6adda59c17ef5563481a3570933f2
David Haubenschild <haubie@gmail.com>
2023-07-12 20:57:48 +1000

    Removed triq and test older test suite

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule Osc.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :osc,
+    [
+      app: :osc,
      version: "0.1.1",
      elixir: "~> 1.0",
      description: "OSC encoder/decoder for elixir",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     package: package,]
+     deps: deps(),
+     package: package()
+    ]
   end
 
   def application do
@@ -17,14 +19,15 @@ defmodule Osc.Mixfile do
   end
 
   defp deps do
-    [{:excheck, "~> 0.3.2", only: [:dev, :test]},
-     {:triq, github: "krestenkrab/triq", only: [:dev, :test]}]
+    []
   end
 
   defp package do
-    [files: ["lib", "mix.exs", "README*"],
-     maintainers: ["Cameron Bytheway"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/camshaft/osc_ex"}]
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Cameron Bytheway"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/camshaft/osc_ex"}
+    ]
   end
 end
